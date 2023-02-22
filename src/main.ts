a23560ace80eee9464a19da5a4cbc71fa7ed8f74
raecoo <raecoo@gmail.com>
2023-02-23 00:09:04 +0800

    Add shopify app-bridge@3

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,23 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import { createApp as createAppBridge } from "@shopify/app-bridge";
+// import { getSessionToken } from "@shopify/app-bridge-utils";
+
+
 import "./assets/main.css";
 
 const app = createApp(App);
+
+const shopifyHost = new URLSearchParams(location.search).get("host");
+
+const appBridge = createAppBridge({
+  apiKey: "e93b19f4d725828de9f94e7619d834e6",
+  host: window.btoa(shopifyHost + "/admin") || "",
+  forceRedirect: false,
+});
+
+app.config.globalProperties.$appBridge = appBridge;
 
 app.use(createPinia());
 app.use(router);
---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <button @click="login">Log in with Shopify</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  methods: {
+    login() {
+      const redirectUrl = window.location.origin + '/auth/callback'
+      const returnUrl = window.location.origin + '/dashboard'
+
+      this.$appBridge.dispatch('Redirect', {
+        path: '/auth',
+        query: {
+          redirectUrl,
+          returnUrl,
+        },
+      })
+    },
+  },
+})
+</script>
